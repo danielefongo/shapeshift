c81b90c7ac5cdeaf278a4323a17cd1777dc190e6
danielefongo <danielefongo@gmail.com>
2019-05-09 14:54:34 +0200

    split git functions to create aggregate results

--- git.zsh
+++ git.zsh
@@ -1,6 +1,4 @@
 DIFF_SYMBOL="-"
-GIT_PROMPT_PREFIX=" "
-GIT_PROMPT_SUFFIX=""
 GIT_PROMPT_AHEAD="%{$fg[cyan]%}%B+NUM%b%{$reset_color%}"
 GIT_PROMPT_BEHIND="%{$fg[cyan]%}%B-NUM%b%{$reset_color%}"
 GIT_PROMPT_MERGING="%{$fg[cyan]%}%Bx%b%{$reset_color%}"
@@ -11,81 +9,60 @@ GIT_PROMPT_DETACHED="%{$fg[neon]%}%B!%b%{$reset_color%}"
 
 function git_branch() {
     branch=$(git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
-    echo ${branch/(refs\/heads|tags)\//}
-}
-
-function git_detached() {
-    if ! git symbolic-ref HEAD >/dev/null 2>&1; then
-        echo "Y"
+    if [[ $branch ]]; then
+        git_where="${branch/(refs\/heads|tags)\//}"
+        echo "%{$fg[white]%}%B${git_where}%b"
     fi
 }
 
-function git_behind() {
-    echo $(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')
-}
-
-function git_ahead() {
-    echo $(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')
-}
-
-function git_untracked() {
+function git_diffs() {
     if [[ $(git ls-files --other --exclude-standard :/ 2> /dev/null) ]] then
-      echo "Y"
+        GIT_DIFFS=$GIT_PROMPT_UNTRACKED
     fi
-}
 
-function git_merge_dir() {
-    echo "$(git rev-parse --git-dir 2> /dev/null)/MERGE_HEAD"
-}
-
-function git_modified() {
     if ! git diff --quiet 2> /dev/null; then
-        echo "Y"
+        GIT_DIFFS=$GIT_DIFFS$GIT_PROMPT_MODIFIED
     fi
-}
 
-function git_staged() {
     if ! git diff --cached --quiet 2> /dev/null; then
-        echo "Y"
+        GIT_DIFFS=$GIT_DIFFS$GIT_PROMPT_STAGED
+    fi
+
+    if [[ $GIT_DIFFS ]]; then
+        echo "$GIT_DIFFS "
     fi
 }
 
-function git_state() {
-    local GIT_STATE=""
-
-    local NUM_AHEAD="$(git_ahead)"
+function git_position() {
+    local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
     if [ "$NUM_AHEAD" -gt 0 ]; then
-        GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
+        GIT_POSITION=$GIT_POSITION${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
     fi
 
-    local NUM_BEHIND="$(git_behind)"
+    local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
     if [ "$NUM_BEHIND" -gt 0 ]; then
-        GIT_STATE="$GIT_STATE ${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}"
+        GIT_POSITION="$GIT_POSITION ${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}"
     fi
 
-    local GIT_MERGE_DIR="$(git_merge_dir)"
+    if ! git symbolic-ref HEAD >/dev/null 2>&1; then
+        local GIT_POSITION="${GIT_PROMPT_DETACHED}"
+    fi
+
+    if [[ $GIT_POSITION ]]; then
+        echo "$GIT_POSITION "
+    fi
+}
+
+function git_merging() {
+    local GIT_MERGE_DIR="$(git rev-parse --git-dir 2> /dev/null)/MERGE_HEAD"
     if [ -f $GIT_MERGE_DIR ]; then
-      GIT_STATE="$GIT_STATE $GIT_PROMPT_MERGING"
+      echo "$GIT_PROMPT_MERGING"
     fi
+}
 
-    if [[ -n "$(git_untracked)" ]]; then
-      GIT_DIFF=$GIT_PROMPT_UNTRACKED
-    fi
-
-    if [[ "$(git_modified)" ]]; then
-      GIT_DIFF=$GIT_DIFF$GIT_PROMPT_MODIFIED
-    fi
-
-    if [[ "$(git_staged)" ]]; then
-      GIT_DIFF=$GIT_DIFF$GIT_PROMPT_STAGED
-    fi
-
-    if [[ -n $GIT_STATE && -n $GIT_DIFF ]]; then
-      GIT_STATE="$GIT_STATE "
-    fi
-    GIT_STATE="$GIT_STATE$GIT_DIFF"
-
-    if [[ -n $GIT_STATE ]]; then
-      echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
+function git_full() {
+    local branch="$(git_branch)"
+    if [[ $branch ]]; then
+        echo "$(git_position)$(git_merging)$(git_diffs)$(git_branch)"
     fi
 }
--- zpure.zsh
+++ zpure.zsh
@@ -9,21 +9,13 @@ function prompt_dir_and_arrow() {
     echo "$(prompt_dir) $(prompt_arrow) "
 }
 
-function git_prompt_string() {
-    local git_where="$(git_branch)"
-    if [[ $(git_detached) ]]; then
-        local git_detached="${GIT_PROMPT_DETACHED}"
-    fi
-    [ -n "$git_where" ] && echo " $(git_state)$GIT_PROMPT_PREFIX%{$fg[white]%}%B${git_where}%b$git_detached$GIT_PROMPT_SUFFIX"
-}
-
 # Prompt
 function PCMD() {
     echo "$(prompt_dir_and_arrow)"
 }
 
 function RCMD() {
-    echo "$(git_prompt_string)"
+    echo "$(git_full)"
 }
 
 PROMPT='$(PCMD)' # single quotes to prevent immediate execution
