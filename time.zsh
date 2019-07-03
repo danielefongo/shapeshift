0a8bd7698c528082c18911069700704db55755dc
danielefongo <danielefongo@gmail.com>
2019-07-03 18:23:27 +0200

    use approximate time instead of total elapsed seconds for command duration

--- time.zsh
+++ time.zsh
@@ -2,16 +2,27 @@ zmodload zsh/datetime
 
 typeset -g timerTimeStamp
 
-last_command_elapsed_seconds() {
-  timer_get
+last_command_elapsed_time() {
+  local millisecondsElapsed=$(timer_get)
+
+  local hours=$(( millisecondsElapsed / 1000 / 60 / 60 % 24 ))
+  local minutes=$(( millisecondsElapsed / 1000 / 60 % 60 ))
+  local seconds=$(( millisecondsElapsed / 1000 % 60 ))
+  local milliseconds=$(( millisecondsElapsed % 1000 ))
+
+  local approxTime
+  (( milliseconds > 0 )) && approxTime="${milliseconds}ms"
+  (( seconds > 0 )) && approxTime="${seconds}s"
+  (( minutes > 0 )) && approxTime="${minutes}m"
+  (( hours > 0 )) && approxTime="${hours}h"
+
+  colorize "$approxTime" $SHAPESHIFT_LAST_COMMAND_ELAPSED_SECONDS_COLOR $SHAPESHIFT_LAST_COMMAND_ELAPSED_SECONDS_BOLD
 }
 
 timer_get() {
-  integer elapsedSeconds
-  (( elapsedSeconds = EPOCHREALTIME - ${timerTimeStamp:-$EPOCHREALTIME} ))
-  if [[ $elapsedSeconds -gt 0 ]]; then
-    colorize "${elapsedSeconds}s" $SHAPESHIFT_LAST_COMMAND_ELAPSED_SECONDS_COLOR $SHAPESHIFT_LAST_COMMAND_ELAPSED_SECONDS_BOLD
-  fi
+  integer milliseconds
+  (( milliseconds = (EPOCHREALTIME - ${timerTimeStamp:-$EPOCHREALTIME})*1000 ))
+  echo $milliseconds
 }
 
 timer_end() {
